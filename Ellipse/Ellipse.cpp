87980930259b379e1e2451832b3f237c713528b0
MrChickenNuggetBM <eeymg2@nottingham.ac.uk>
2023-11-19 11:41:40 +0000

    completed first draft of Ellipse class

--- Ellipse/Ellipse.cpp
+++ Ellipse/Ellipse.cpp
@@ -1,24 +1,100 @@
-#ifndef ELLIPSE_H
-#define ELLIPSE_H
+#include "Ellipse.h"
+using namespace cv;
+using namespace std;
 
-#include <string>
-#include <utility>
+// constructors
+Ellipse::Ellipse(const Point2f& center, const Size2f& size, float angle, cv::Scalar newColour, int newThickness) :
+	RotatedRect(center, size, angle),
+	colour(newColour),
+	thickness(newThickness) {}
 
-template<typename radiiType, typename posType>
-class Ellipse
+Ellipse::Ellipse(const Ellipse& copiedEllipse) :
+	RotatedRect(copiedEllipse.center, copiedEllipse.size, copiedEllipse.angle),
+	colour(copiedEllipse.colour),
+	thickness(copiedEllipse.thickness) {}
+
+// destructor
+Ellipse::~Ellipse() {}
+
+// operator overloads
+Ellipse& Ellipse::operator=(const Ellipse& assignedEllipse)
 {
-private:
-	// constructors
-	Ellipse(radiiType newMinorRadius, radiiType newMajorRadius, pair<posType, posType> newCenter);
-	Ellipse(String newName, radiiType newMinorRadius, radiiType newMajorRadius, pair<posType, posType> newCenter);
-	Ellipse(Ellipse& newEllipse);
+	center = assignedEllipse.center;
+	size = assignedEllipse.size;
+	angle = assignedEllipse.angle;
+	return (*this);
+}
 
-	// deconstructor
-	~Ellipse();
+// gets
+float Ellipse::minorRadius() const { return (min(size.width, size.height) / 2.0f); }
 
-	// operator overloads
-	Ellipse& operator=(Ellipse& assignedEllipse);
-public:
-};
+float Ellipse::majorRadius() const { return (max(size.width, size.height) / 2.0f); }
 
-#endif // ELLIPSE_H
+float Ellipse::eccentricity() const {
+	float minRad(minorRadius());
+	float majRad(majorRadius());
+	return (sqrt(1 - ((minRad * minRad) / (majRad * majRad))));
+}
+
+float Ellipse::area() const
+{
+	return(CV_PI * minorRadius() * majorRadius());
+}
+
+bool Ellipse::isPointInside(const Point2f& point)
+{
+	Rect_<float> boundingRect = boundingRect2f();
+	if (!boundingRect.contains(point))
+		return(false);
+
+	float a = minorRadius();
+	float b = majorRadius();
+	float xDiff = point.x - center.x;
+	float yDiff = point.y - center.y;
+
+
+	return((xDiff * xDiff) / (a * a) + (yDiff * yDiff) / (b * b) <= 1.0f);
+}
+
+bool Ellipse::isCircle()
+{
+	return(eccentricity() <= 0.2f);
+}
+
+void Ellipse::translate(float xTranslation, float yTranslation)
+{
+	center.x += xTranslation;
+	center.y += yTranslation;
+}
+void Ellipse::operator+(float xyTranslation)
+{
+	translate(xyTranslation, xyTranslation);
+}
+
+void Ellipse::scale(float xScale, float yScale)
+{
+	size.width *= xScale;
+	size.height *= yScale;
+}
+void Ellipse::operator*(float xyScale)
+{
+	scale(xyScale, xyScale);
+}
+
+void Ellipse::rotate(float angleDeg)
+{
+	angle += angleDeg;
+}
+void Ellipse::rotateRad(float angleRad)
+{
+	rotate(angleRad * 180 / CV_PI);
+}
+
+void Ellipse::draw(Mat frame)
+{
+	ellipse(frame, *this, colour, thickness);
+}
+void Ellipse::operator()(Mat frame)
+{
+	draw(frame);
+}
--- Ellipse/Ellipse.h
+++ Ellipse/Ellipse.h
@@ -1,30 +1,45 @@
 #ifndef ELLIPSE_H
 #define ELLIPSE_H
 
-#include <string>
-#include <utility>
+#include <opencv2/opencv.hpp>
 
-template<typename radiiType, typename posType>
-class Ellipse
+class Ellipse : public cv::RotatedRect
 {
-private:
-	std::string name;
-	std::pair<posType, posType> center;
-	radiiType minorRadius;
-	radiiType majorRadius;
-	
 public:
 	// constructors
-	Ellipse(radiiType newMinorRadius, radiiType newMajorRadius, std::pair<posType, posType> newCenter);
-	Ellipse(std::string newName, radiiType newMinorRadius, radiiType newMajorRadius, std::pair<posType, posType> newCenter);
-	Ellipse(Ellipse& newEllipse);
+	Ellipse(const cv::Point2f& center, const cv::Size2f& size, float angle, cv::Scalar newColour = (255, 255, 255), int newThickness = 1);
+	Ellipse(const Ellipse& newEllipse);
 
-	// deconstructor
+	// destructor
 	~Ellipse();
 
 	// operator overloads
-	Ellipse& operator=(Ellipse& assignedEllipse);
+	Ellipse& operator=(const Ellipse& assignedEllipse);
 
+	// public variables to store the colour and thickness of the ellipse
+	cv::Scalar colour;
+	int thickness;
+
+	// gets
+	float minorRadius() const;
+	float majorRadius() const;
+	float eccentricity() const;
+	float area() const;
+
+	bool isPointInside(const cv::Point2f& point);
+	bool isCircle();
+
+	void translate(float xTranslation, float yTranslation);
+	void operator+(float xyTranslation);
+
+	void scale(float xScale, float yScale);
+	void operator*(float xyScale);
+
+	void rotate(float angleDeg);
+	void rotateRad(float angleRad);
+
+	void draw(cv::Mat frame);
+	void operator()(cv::Mat frame);
 };
 
 #endif // ELLIPSE_H
