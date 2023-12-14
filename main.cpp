2f7fc1407687aeaf44114baacce2de33cdea050e
MrChickenNuggetBM <eeymg2@nottingham.ac.uk>
2023-12-14 12:56:10 +0000

    hi

--- main.cpp
+++ main.cpp
@@ -14,55 +14,31 @@
 //     return true;
 // }
 
-using namespace cv;
-using namespace std;
 
 int main() {
-    // 📸 Capture an image with OpenCV (replace this with your image capture logic)
-    Mat image = imread("your_image.jpg");
+    // 📸 Image file path
+    const char* imageFilePath = "your_image.jpg";
 
+    // 🚨 Check if the image file exists
+    if (access(imageFilePath, F_OK) == -1) {
+        cerr << "Error: Image file not found" << endl;
+        return -1;
+    }
+
+    // 📸 Read the image
+    Mat image = imread(imageFilePath);
+    
     // 🚀 Assuming your second display is /dev/fb1 (adjust if needed)
-    const char* framebuffer_path = "/dev/fb1";
+    const char* framebufferPath = "/dev/fb1";
 
     // 📏 Get framebuffer information
-    int fb = open(framebuffer_path, O_RDWR);
+    int fb = open(framebufferPath, O_RDWR);
     if (fb == -1) {
         cerr << "Error opening framebuffer device" << endl;
         return -1;
     }
 
-    struct fb_var_screeninfo vinfo;
-    if (ioctl(fb, FBIOGET_VSCREENINFO, &vinfo)) {
-        cerr << "Error reading variable information" << endl;
-        close(fb);
-        return -1;
-    }
-
-    // 🖼️ Calculate the size of the framebuffer
-    size_t screensize = vinfo.yres_virtual * vinfo.xres_virtual * vinfo.bits_per_pixel / 8;
-
-    // 🖼️ Map framebuffer to user space
-    unsigned char* framebuffer = (unsigned char*)mmap(nullptr, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb, 0);
-    if (framebuffer == MAP_FAILED) {
-        cerr << "Error mapping framebuffer device to memory" << endl;
-        close(fb);
-        return -1;
-    }
-
-    // 🖼️ Copy OpenCV image to the framebuffer
-    for (int y = 0; y < image.rows; ++y) {
-        for (int x = 0; x < image.cols; ++x) {
-            // 🎨 Assuming your image is in BGR format
-            unsigned char* pixel = &framebuffer[(vinfo.yoffset + y) * vinfo.xres_virtual * 4 + (vinfo.xoffset + x) * 4];
-            pixel[0] = image.at<Vec3b>(y, x)[0]; // Blue
-            pixel[1] = image.at<Vec3b>(y, x)[1]; // Green
-            pixel[2] = image.at<Vec3b>(y, x)[2]; // Red
-            pixel[3] = 0; // Alpha (transparency)
-        }
-    }
-
-    // 🚀 Optionally, you can add a delay to display the image for some time
-    usleep(5000000); // 5 seconds
+    // ... (Rest of the code remains unchanged)
 
     // 🔄 Cleanup and close the framebuffer
     munmap(framebuffer, screensize);
