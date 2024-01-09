1b9d6450019630e51863a7016cee1ff054332225
MrChickenNuggetBM <eeymg2@nottingham.ac.uk>
2024-01-09 16:17:28 +0000

    sus

--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
 #include "main.h"
 
-
-
-string SERVER_ADDRESS("mqtt://localhost:1883");
+// topics to subscribe to
 string TOPICS[]  = {"parameters/minorRadius"};
-
+// mqtt broker definition
+string SERVER_ADDRESS("mqtt://localhost:1883");
 async_client cli(SERVER_ADDRESS, "raspberrypi");
 
-int main(int argc, char* argv[]) {
-    // A subscriber often wants the server to remember its messages when its
-    // disconnected. In that case, it needs a unique ClientID and a
-    // non-clean session.
+// displays an image on the specified frame buffer
+bool display(Mat &image)
+{
+    ofstream frameBuffer("/dev/fb0", ios::binary);
 
+    if (!frameBuffer.is_open())
+    {
+        cerr << "Error: Unable to open framebuffer device." << endl;
+        return false;
+    }
 
+    frameBuffer.write(reinterpret_cast<char *>(image.data), static_cast<streamsize>(image.total() * image.elemSize()));
 
+    frameBuffer.close();
+
+    return true;
+}
+
+bool setup()
+{
+    // clear the terminal
+    system("setterm -cursor off;clear");
+
+    // set up the camera
+    // if (!videoCapture.isOpened())
+    // {
+    //     cerr << "Error: Could not open camera" << endl;
+    //     return false;
+    // }
+
+    // videoCapture.set(CAP_PROP_FRAME_WIDTH, 960);
+    // videoCapture.set(CAP_PROP_FRAME_HEIGHT, 540);
+
+    // configure code termination
+    atexit(teardown);
+    signal(SIGINT, teardown);
+
+    // establish broker-client connection
     connect_options connOpts;
     connOpts.set_clean_session(false);
 
@@ -34,10 +64,40 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Just block till user tells us to quit.
+    return true;
+}
 
-    while (tolower(cin.get()) != 'q')
-        ;
+bool loop()
+{
+    // Mat cameraImage;
+    // videoCapture.read(cameraImage);
+
+    Mat frame(
+            1080,
+            1920,
+            CV_8UC4,
+            Scalar(0, 0, 0, 255));
+
+    Ellipse ellipse(
+            Point2f(960, 540),
+            Size2f(
+                    1920 - 10. * float(i % 100),
+                    1080 - 10. * float(i % 100)),
+            0,
+            Scalar(255, 255, 255),
+            3);
+    ellipse(frame);
+
+    // imshow("hi", cameraImage);
+    return (/* display(frame) && */(waitKey(1) < 0));
+}
+
+void teardown()
+{
+    system("setterm -cursor on;clear");
+
+    cout << endl
+         << "Stopped after " << i << " frames" << endl;
 
     // Disconnect
 
@@ -51,5 +111,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    return 0;
+    // videoCapture.release();
+
+    // destroyAllWindows();
+}
+
+void teardown(int signal)
+{
+    exit(EXIT_SUCCESS);
 }
